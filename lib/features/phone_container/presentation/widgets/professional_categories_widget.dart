e63a15c9f904ed63066fb2766432b89343075fb5
@tiagodicquemare <tdicquemare@gmail.com>
2023-12-15 15:38:35 +0100

    fix spacing

--- lib/features/phone_container/presentation/widgets/professional_categories_widget.dart
+++ lib/features/phone_container/presentation/widgets/professional_categories_widget.dart
@@ -1,3 +1,4 @@
+import 'package:dicquemare_solution/core/ui/text_styles.dart';
 import 'package:dicquemare_solution/features/phone_container/presentation/bloc/phone_container_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -15,40 +16,77 @@ class ProfessionalCategoriesWidget extends StatefulWidget {
 class _ProfessionalCategoriesWidgetState
     extends State<ProfessionalCategoriesWidget> {
   final categories = [
-    'Bio',
+    'Introduction',
     'Chronologie',
-    'CV',
+    'Curriculum Vitae',
     'Stack technique',
     'Recommandations'
   ];
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 24,
-        ),
-        const Text("Dicquemare Solution",
-            style: TextStyle(
-              fontSize: 26,
-              fontWeight: FontWeight.bold,
-            )),
-        const SizedBox(
-          height: 48,
-        ),
-        ListView.separated(
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 48,
+          ),
+          Text(" Catégories",
+              style: AppTextStyles.textXXLSemiBold(color: Colors.black)),
+          const SizedBox(
+            height: 24,
+          ),
+          ListView.separated(
             physics: ClampingScrollPhysics(),
             shrinkWrap: true,
             itemCount: categories.length,
             itemBuilder: ((context, index) {
-              return categoryWidget(categories[index], index);
+              return newCategoryWidget(categories[index], index);
             }),
             separatorBuilder: ((context, index) {
               return const SizedBox(
-                height: 16,
+                height: 24,
               );
-            })),
-      ],
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget newCategoryWidget(String category, int index) {
+    return InkWell(
+      onTap: () {
+        BlocProvider.of<PhoneContainerBloc>(context)
+            .add(ShowProfessionalCategoriesEvent(category: index + 1));
+      },
+      child: Container(
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 1,
+              blurRadius: 10,
+              offset: const Offset(0, 0),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            const SizedBox(
+              width: 32,
+            ),
+            Text(
+              category,
+              style: AppTextStyles.textLRegular(color: Colors.black),
+            )
+          ],
+        ),
+      ),
     );
   }
 
